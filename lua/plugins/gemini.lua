4a00b66339e2e67fa85c31bfc31c8b5d9ae1f893
henriwasd <henriwasd@gmail.com>
2024-10-25 12:07:58 -0300

    lazy transparent

---
+++ lua/plugins/gemini.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "kiddos/gemini.nvim",
+    build = { "pip install -r requirements.txt", ":UpdateRemotePlugins" },
+    config = function()
+      require("gemini").setup()
+    end,
+  },
+}
