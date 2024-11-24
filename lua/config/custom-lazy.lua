c4bdff30e5e3359f60c6ff599d5a6b498b3a45ef
hen <henriwasd@gmail.com>
2024-11-24 01:46:18 -0300

    add lazyclip

--- lua/config/custom-lazy.lua
+++ lua/config/custom-lazy.lua
@@ -5,14 +5,11 @@ end
 
 custom.setup({
   spec = {
-    -- { import = "lazyvim.plugins.extras.vscode" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.prisma" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
-    -- { import = "lazyvim.plugins.extras.coding.copilot" },
-    -- { import = "lazyvim.plugins.extras.coding.copilot-chat" },
   },
 })
