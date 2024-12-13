d4b177bb8b579abac6d4879c24f544a1ebdb1011
henriwasd <henriwasd@gmail.com>
2024-12-13 10:11:21 -0300

    flutter change

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -50,27 +50,6 @@ return {
             detached = false,
           },
         }
-        dap.configurations.dart = {
-          {
-            type = "dart",
-            request = "launch",
-            name = "Launch dart",
-            dartSdkPath = "/opt/flutter/bin/cache/dart-sdk/bin/dart", -- ensure this is correct
-            flutterSdkPath = "/opt/flutter/bin/flutter", -- ensure this is correct
-            program = "${workspaceFolder}/lib/main.dart", -- ensure this is correct
-            cwd = "${workspaceFolder}",
-          },
-          {
-            type = "flutter",
-            request = "launch",
-            name = "Launch flutter",
-            dartSdkPath = "/opt/flutter/bin/cache/dart-sdk/bin/dart", -- ensure this is correct
-            flutterSdkPath = "/opt/flutter/bin/flutter", -- ensure this is correct
-            program = "${workspaceFolder}/lib/main.dart", -- ensure this is correct
-            cwd = "${workspaceFolder}",
-          },
-        }
-        require("dap.ext.vscode").load_launchjs()
       end,
     },
   },
