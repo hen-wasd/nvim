92bd821a5503b21ec408ca1cff68371ac177d13c
hen <henriwasd@gmail.com>
2025-01-10 10:41:17 -0300

    clip shared wsl2 + windows

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,3 +1,19 @@
+local clip = "/mnt/c/Windows/System32/clip.exe" -- Change this path if needed
+
+if vim.fn.executable(clip) then
+  local opts = {
+    callback = function()
+      if vim.v.event.operator ~= "y" then
+        return
+      end
+      vim.fn.system(clip, vim.fn.getreg(0))
+    end,
+  }
+
+  opts.group = vim.api.nvim_create_augroup("WSLYank", {})
+  vim.api.nvim_create_autocmd("TextYankPost", { group = opts.group, callback = opts.callback })
+end
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
