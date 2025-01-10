a3eb241ee3c783cd0a2342a552e4cd3f413474ad
hen <henriwasd@gmail.com>
2025-01-10 08:50:28 -0300

    theme minimalist

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -13,9 +13,39 @@ return {
   },
 
   {
+    "jackplus-xyz/binary.nvim",
+    opts = {
+      style = "dark",
+      -- colors = {
+      --   fg = "#FFB400",
+      -- },
+      -- Customize the reversed groups
+      reversed_group = {
+        Title = true, -- Add a new reversed group
+        Search = false, -- Disable a default reversed group
+      },
+    },
+  },
+  {
+    "zenbones-theme/zenbones.nvim",
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    dependencies = "rktjmp/lush.nvim",
+    lazy = false,
+    priority = 1000,
+    -- you can set set configuration options here
+    -- config = function()
+    --     vim.g.zenbones_darken_comments = 45
+    --     vim.cmd.colorscheme('zenbones')
+    -- end
+  },
+
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "material-darker",
+      colorscheme = "zenbones",
+      background = "dark",
     },
   },
 }
